6ed4b2e5db2121798f3b1750dc7a9abfb0ff7ebf
kvnlay <kingstevens007@gmail.com>
2019-11-22 05:39:23 +0100

    refactored components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
+import MentorDetail from "./components/MentorDetail";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/mentors/:publicId" component={MentorDetail} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/MentorCard.js
+++ src/components/MentorCard.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function MentorCard() {
-  return <div></div>;
+export default function MentorCard(props) {
+  const { name, professionalHeadline, publicId } = props.bio;
+  let url = `/mentors/${publicId}`;
+  return (
+    <div className="card mb-2 bg-light " style={{ width: "18rem" }}>
+      {/* <img className="card-img-top" src="..." alt="cap" /> */}
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{professionalHeadline}</p>
+        <Link to={url} className="btn btn-primary">
+          View profile
+        </Link>
+      </div>
+    </div>
+  );
 }
