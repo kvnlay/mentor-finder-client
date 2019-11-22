7cc4cec8b2dfbc3592ae51db4217efb6fb86cb71
kvnlay <kingstevens007@gmail.com>
2019-11-22 05:35:50 +0100

    Add bio and connections

--- src/components/MentorList.js
+++ src/components/MentorList.js
@@ -1,5 +1,14 @@
 import React from "react";
+import MentorCard from "./MentorCard";
 
-export default function MentorList() {
-  return <div></div>;
+function MentorList({ bios }) {
+  console.log(bios);
+  return (
+    <div className="d-flex justify-content-around flex-wrap px-2 mb-5">
+      {bios &&
+        bios.map(bio => <MentorCard key={bio.id || bio.person.id} bio={bio} />)}
+    </div>
+  );
 }
+
+export default MentorList;
